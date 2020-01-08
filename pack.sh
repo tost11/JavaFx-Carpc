eb570bb059b519376c57f0c1524878c0afe929c9
tost <tost@tost-soft.de>
2020-01-09 00:21:15 +0100

    fixed pack scripts and added installation skcrip for openjdk11

--- pack.sh
+++ pack.sh
@@ -5,11 +5,12 @@ mkdir build
 
 git submodule update --init --recursive
 
-javac -d build -sourcepath 'src/main/java/:json-simple/src/main/java/'  src/main/java/de/tostsoft/carpc/PlayerMain.java
+javac -d build -sourcepath 'src/main/java/:json-simple/src/main/java/:java-mpd/src/main/java/'  src/main/java/de/tostsoft/carpc/PlayerMain.java
 
 echo "-> build new executable jar file"
 cd build
 
 echo "Main-Class: de.tostsoft.carpc.PlayerMain" > manifest.txt
 echo "Class-Path: ." >> manifest.txt
+echo "" >> manifest.txt
 jar -cvfm Player.jar manifest.txt
--- pack_pi4.sh
+++ pack_pi4.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-PATH_TO_FX=/home/pi/armv6hf-sdk/lib/
+#PATH_TO_FX=/home/pi/armv6hf-sdk/lib/
+#PATH_TO_FX=/usr/share/openjfx/lib
 
 rm -R build
 mkdir build
 
 git submodule update --init --recursive
 
-javac --module-path $PATH_TO_FX --add-modules=javafx.controls -d build -sourcepath 'src/main/java/:json-simple/src/main/java/' src/main/java/de/tostsoft/carpc/PlayerMain.java
+javac --module-path $PATH_TO_FX --add-modules javafx.controls -d build -sourcepath 'src/main/java/:json-simple/src/main/java/:java-mpd/src/main/java/' src/main/java/de/tostsoft/carpc/PlayerMain.java
 
 echo "-> build new executable jar file"
 cd build
